33ee76adc658258bde41f9daf55e65f715c834e5
VDharani004 <dharaniv444@gmail.com>
2026-06-14 13:58:08 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,10 +22,10 @@ export default function App() {
       setLoading(true);
 
       const response = await axios.post(
-        "http://127.0.0.1:8000/generate",
-        {
+         `${import.meta.env.VITE_API_URL}/generate`,
+         {
           prompt,
-        }
+          }
       );
 
       setResult(response.data);
